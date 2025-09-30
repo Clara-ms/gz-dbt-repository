c9d781a449af5d2c941ea7a13719add2c20111bb
Clara Martín <cmartin.santasusana@gmail.com>
2025-09-30 13:52:49 +0000

    Added intermediate model for operational margin per order

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,6 @@
+SELECT
+    orders.*,
+    ROUND(orders.margin+ship.shipping_fee-ship.log_cost-ship.ship_cost,2) AS operational_margin
+FROM {{ ref('int_orders_margin') }} AS orders
+LEFT JOIN {{ ref('stg_raw__ship') }} AS ship
+USING(orders_id)
