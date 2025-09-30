2a52833e7f55efde6325fb6bc693f708f441fcfc
Clara Martín <cmartin.santasusana@gmail.com>
2025-09-30 14:12:16 +0000

    Created mart model for finance team

---
+++ models/mart/finance_days.sql
@@ -0,0 +1,11 @@
+SELECT
+    date_date,
+    COUNT(orders_id) AS nb_transactions,
+    ROUND(SUM(revenue),2) AS total_revenue,
+    ROUND(SAFE_DIVIDE(SUM(revenue),COUNT(orders_id)),2) AS avg_basket,
+    ROUND(SUM(operational_margin),2) AS operational_margin,
+    ROUND(SUM(purchase_cost),2) AS total_purchase_cost,
+    ROUND(SUM(log_cost),2) AS total_log_cost,
+    SUM(quantity) AS total_quantity_products
+FROM {{ ref('int_orders_operational') }}
+GROUP BY date_date
