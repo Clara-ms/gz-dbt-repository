f6d90fcecffb5b599b8c682958af9ac8885af264
Clara Martín <cmartin.santasusana@gmail.com>
2025-09-30 13:03:24 +0000

    Added intermediate model with margin per product

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,13 @@
+WITH cost AS(
+    SELECT 
+        sales.*,
+        product.purchase_price,
+        ROUND(sales.quantity*product.purchase_price,2) AS purchase_cost   
+    FROM {{ ref('stg_raw__sales') }} AS sales
+    LEFT JOIN {{ ref('stg_raw__product') }} AS product
+    ON sales.products_id=product.products_id
+)
+SELECT
+    *,
+    ROUND(revenue-purchase_cost,2) AS margin
+FROM cost
